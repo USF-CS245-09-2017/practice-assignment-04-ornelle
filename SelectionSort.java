b955e5f4f78ab9b2e61f18f0260ef16ecce768e6
ornelle <42754183+ornelle@users.noreply.github.com>
2019-02-21 23:21:18 -0800

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -4,7 +4,7 @@ public class SelectionSort implements SortingAlgorithm {
         for(int i=0; i<a.length-1; i++)
         {
             int min= i;
-            for(int j=i+1; j<a.length; i++)
+            for(int j=i+1; j<a.length; j++)
             {
                 if(a[j]<a[min])
                     min=j;
