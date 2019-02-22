686c8c4a06f2a7b04d35484eb4877a67c66ff922
ornelle <42754183+ornelle@users.noreply.github.com>
2019-02-21 23:15:10 -0800

    done

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,12 +1,14 @@
 
 public class BubbleSort implements SortingAlgorithm{
-    void sort(int []a){
-        boolean swapped= True;
+    public void sort(int[] a){
+        boolean swapped= true;
         for(int i=0; i< a.length-1; i++)
         {
-            for(int j=0; i< a.length-1-i;j++){
-                if(a[i]>a[i+1]) {
-                    swap(a, i, i + 1);
+            for(int j=0; j< a.length-1-i;j++){
+                if(a[j]>a[j+1]) {
+                    int temp = a[j];
+                    a[j]= a[j+1];
+                    a[j+1]=temp;
                     swapped = true;
                 }
             }
